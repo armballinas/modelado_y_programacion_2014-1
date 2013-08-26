c485d1c106ddf84cda138209da3ffa27ced8a4b6
Armando <armando@localhost.localdomain>
2013-08-26 18:47:07 -0500

    moviendo el repo a github

--- ejemplosyprogramasC/funcads.c
+++ ejemplosyprogramasC/funcads.c
@@ -1,12 +1,27 @@
 #include<stdio.h>
-#define MAXLINE 1000
+#include "funcads.h"
 
-int getline(char line[], int maxline);
-void copy(char to[], char from[]);
 
-int main(){
-	int len;
-	int max;
-	char line[MAXLINE];
-
+int migetline(char line[], int maxline){
+	int c,i;
+	
+	for(i=0;i<maxline-1 && (c = getchar()) != EOF && c!='\n';++i){
+		line[i] = c;
+	}
+	if(c=='\n'){
+		line[i] = c;
+		++i;
+	}
+	line[i] = '\0';
+	return i;
 }
+
+void copy(char to[], char from[]){
+	int i;
+	i = 0;
+	while((to[i] = from[i]) != '\0'){
+		i++;
+	}
+}
+
+
